4437846dc4d9a5d32b59af375ba839a78ee5e7db
Igor <igorsup@gmail.com>
2026-05-18 16:54:09 +0300

    Fix Astro base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://igortau.github.io',
+  base: '/portfolio-site',
+
   vite: {
     plugins: [tailwindcss()]
   }
